aa3d3b21dfced97915458c20cdb838b817ac6451
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-05-05 23:08:14 +0600

    Delete examples.py

--- src/examples.py
+++
@@ -1,5 +0,0 @@
-# simple login
-import anixart
-anixclient = anixart.AniXClient()
-anixclient.login(login="email or login", password="password")
-print(f"-- Account user_id::: {anixclient.user_id}")
